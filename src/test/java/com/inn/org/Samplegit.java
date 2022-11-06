690fd04b5a8b923a558b707b26599ae8768d2e45
Hariharaveervicky <hari.info2vicky.tech@gmail.com>
2022-11-06 20:51:12 +0530

    first push

---
+++ src/test/java/com/inn/org/Samplegit.java
@@ -0,0 +1,17 @@
+package com.inn.org;
+
+public class Samplegit {
+
+	public void Tool1() {
+		System.out.println("tool1");
+}
+	public void Tool2() {
+		System.out.println("tool2");
+}
+	public void Tool3() {
+System.out.println("tool3");
+}
+	public void Tool4() {
+System.out.println("tool4");
+}
+}
